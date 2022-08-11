beee556f2d3b6f6f8769ac1e106097702871ace9
jeonDev <jjhwhdgus0727@gmail.com>
2022-08-11 13:46:27 +0900

    책서비스 책등록

--- junit-project/src/main/java/com/spring/junitproject/service/BookService.java
+++ junit-project/src/main/java/com/spring/junitproject/service/BookService.java
@@ -1,8 +1,35 @@
 package com.spring.junitproject.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.spring.junitproject.domain.Book;
+import com.spring.junitproject.domain.BookRepository;
+import com.spring.junitproject.web.dto.BookResponseDto;
+import com.spring.junitproject.web.dto.BookSaveReqDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    // 1. 책 등록
+    @Transactional(rollbackFor = RuntimeException.class)
+    public BookResponseDto 책등록하기(BookSaveReqDto dto) {
+        Book bookPs = bookRepository.save(dto.toEntity());
+        return new BookResponseDto().toDto(bookPs);
+    }
     
+
+    // 2. 책목록보기
+
+    // 3. 책 한건 보기
+
+    // 4. 책삭제
+
+    // 5. 책수정
 }
--- junit-project/src/main/java/com/spring/junitproject/web/dto/BookResponseDto.java
+++ junit-project/src/main/java/com/spring/junitproject/web/dto/BookResponseDto.java
@@ -1,5 +1,20 @@
 package com.spring.junitproject.web.dto;
 
+import com.spring.junitproject.domain.Book;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@NoArgsConstructor
+@Getter
 public class BookResponseDto {
-    
+    private Long id;
+    private String title;
+    private String author;
+
+    public BookResponseDto toDto(Book bookPs) {
+        this.id = bookPs.getId();
+        this.title = bookPs.getTitle();
+        this.author = bookPs.getAuthor();
+        return this;
+    }
 }
